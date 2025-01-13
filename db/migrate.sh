9a4952384f68433828db44ad69b7161c37e1669f
Leonardo Gavaudan <leonardogavaudan@gmail.com>
2025-01-13 21:23:53 +0100

    Add CI steps to run migrations on change

--- db/migrate.sh
+++ db/migrate.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 # Get the directory where the script is located
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
@@ -8,13 +9,16 @@ set -a
 source "$SCRIPT_DIR/../.env"
 set +a
 
+# Check if required variables are set
+: "${POSTGRES_USER:?POSTGRES_USER is not set}"
+: "${POSTGRES_PASSWORD:?POSTGRES_PASSWORD is not set}"
+: "${POSTGRES_DB:?POSTGRES_DB is not set}"
+
 # Construct the database URL
 DB_URL="postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@localhost:5432/$POSTGRES_DB?sslmode=disable"
 
 # Run goose migrations
-goose -dir "$SCRIPT_DIR/migrations" postgres "$DB_URL" up
-
-if [ $? -eq 0 ]; then
+if goose -dir "$SCRIPT_DIR/migrations" postgres "$DB_URL" up; then
     echo "✅ Successfully ran all migrations"
 else
     echo "❌ Failed to run migrations"
