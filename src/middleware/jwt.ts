bbb68e08ec8edc97326f85eaaecd6b0c832faa17
Leonardo Gavaudan <leonardogavaudan@gmail.com>
2025-01-16 17:25:40 +0100

    set user context after jwt payload validation

--- src/middleware/jwt.ts
+++ src/middleware/jwt.ts
@@ -1,7 +1,20 @@
+import { Context, Next } from "hono";
 import { jwt } from "hono/jwt";
 
 const JWT_SECRET: string = process.env.JWT_SECRET!;
 
-export const jwtMiddleware = jwt({
+export interface JwtPayload {
+  id: string;
+  email: string;
+}
+
+const verifyJwt = jwt({
   secret: JWT_SECRET,
 });
+
+export const jwtMiddleware = async (c: Context, next: Next) => {
+  await verifyJwt(c, next);
+  const payload = c.get("jwtPayload") as JwtPayload;
+  c.set("user", payload);
+  await next();
+};
