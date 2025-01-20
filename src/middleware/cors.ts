a3933bc3e8796ac046ba6d66df08d0f131e216e1
Leonardo Gavaudan <leonardogavaudan@gmail.com>
2025-01-20 21:15:43 +0100

    Expand allowed headers

--- src/middleware/cors.ts
+++ src/middleware/cors.ts
@@ -1,12 +1,19 @@
-import { MiddlewareHandler } from 'hono'
-import { cors } from 'hono/cors'
+import { MiddlewareHandler } from "hono";
+import { cors } from "hono/cors";
 
 export const corsMiddleware = (): MiddlewareHandler => {
   return cors({
-    origin: 'https://app.lifemetrics.io',
-    allowMethods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowHeaders: ['Authorization', 'Content-Type'],
+    origin: "https://app.lifemetrics.io",
+    allowMethods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowHeaders: [
+      "Authorization",
+      "Content-Type",
+      "Accept",
+      "x-environment",
+      "User-Agent",
+      "Referer",
+    ],
     maxAge: 86400, // 24 hours in seconds
-    credentials: true
-  })
-}
+    credentials: true,
+  });
+};
