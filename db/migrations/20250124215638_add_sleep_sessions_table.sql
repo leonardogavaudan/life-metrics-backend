39caa5084dea8a7c09ddfcfa17b2d4046c842a4c
Leonardo Gavaudan <leonardogavaudan@gmail.com>
2025-02-11 16:44:04 +0100

    switched migrations

--- db/migrations/20250124215638_add_sleep_sessions_table.sql
+++ db/migrations/20250124215638_add_sleep_sessions_table.sql
@@ -1,17 +1,14 @@
 -- +goose Up
 -- +goose StatementBegin
-CREATE TABLE time_series_metrics (
+CREATE TABLE sleep_sessions (
   id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
   user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
-  metric_type VARCHAR(50) NOT NULL,
-  value NUMERIC NOT NULL,
-  unit VARCHAR(50) NOT NULL,
-  event_timestamp TIMESTAMP NOT NULL,
   integration_id UUID REFERENCES integrations(id) ON DELETE CASCADE,
-  sleep_id UUID REFERENCES sleep_sessions(id) ON DELETE SET NULL,
+  start_timestamp TIMESTAMPTZ NOT NULL,
+  end_timestamp TIMESTAMPTZ NOT NULL,
   created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
   updated_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
-  UNIQUE (user_id, event_timestamp, metric_type)
+  UNIQUE (user_id, start_timestamp, integration_id)
 );
 
 CREATE TRIGGER set_timestamp
@@ -22,5 +19,5 @@ CREATE TRIGGER set_timestamp
 
 -- +goose Down
 -- +goose StatementBegin
-DROP TABLE IF EXISTS time_series_metrics;
+DROP TABLE IF EXISTS sleep_sessions;
 -- +goose StatementEnd
