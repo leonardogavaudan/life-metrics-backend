6aeeace5a624692ea5323a0fde1c3fd59252d618
Leonardo Gavaudan <leonardogavaudan@gmail.com>
2025-02-11 18:40:01 +0100

    Add check to make user JWT secret env var existis

--- src/middleware/jwt.ts
+++ src/middleware/jwt.ts
@@ -1,11 +1,14 @@
 import { Context, Next } from "hono";
 import { jwt } from "hono/jwt";
 
-const JWT_SECRET: string = process.env.JWT_SECRET!;
-
 export interface JwtPayload {
   id: string;
   email: string;
+}
+
+const JWT_SECRET: string = process.env.JWT_SECRET!;
+if (!JWT_SECRET) {
+  throw new Error("Missing JWT SECRET");
 }
 
 const verifyJwt = jwt({
