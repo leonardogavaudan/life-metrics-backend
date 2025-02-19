0d3dabc35396c13b331ef9582aece3e99496e9e4
Leonardo Gavaudan <leonardogavaudan@gmail.com>
2025-02-19 15:22:02 +0100

    Make api service wait for rabbitmq healthcheck, invoke setupCronJobs

--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,17 @@ import { usersRouter } from "./routes/users";
 import { integrationsRouter } from "./routes/integrations";
 import { corsMiddleware } from "./middleware/cors";
 import { requestContextMiddleware } from "./middleware/request_context";
+import { setupCronJobs } from "./cron";
 
 const app = new Hono();
 
-app.use("*", requestContextMiddleware)
+app.use("*", requestContextMiddleware);
 app.use("*", corsMiddleware);
 
 app.route("/auth", authRouter);
 app.route("/users", usersRouter);
 app.route("/integrations", integrationsRouter);
+
+setupCronJobs();
 
 export default app;
