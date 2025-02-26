b47d6c0e56b12bed5cc8734884563a954aa8eaf1
Leonardo Gavaudan <leonardogavaudan@gmail.com>
2025-02-26 17:37:44 +0100

    import every 15 mins vs 5 min

--- src/cron/cron.ts
+++ src/cron/cron.ts
@@ -8,14 +8,14 @@ import {
 } from "../messaging/messaging.message";
 
 export function setupCronJobs(): void {
-  schedule("*/5 * * * *", async () => {
+  schedule("*/15 * * * *", async () => {
     const integrations = await getIntegrations();
     const messages = integrations.map((integration) => {
       return createMessage<SyncMetricsMessagePayload>(Queue.SyncMetrics, {
         userId: integration.user_id,
         provider: integration.provider,
         startTime: new Date().toISOString(),
-        endTime: new Date(Date.now() + 5 * 60 * 1000).toISOString(),
+        endTime: new Date(Date.now() + 15 * 60 * 1000).toISOString(),
       });
     });
     await sendMessagesToQueue(Queue.SyncMetrics, messages);
