0d184b64e8a7b4ffb8083f5848e419ce3b97304a
Leonardo Gavaudan <leonardogavaudan@gmail.com>
2025-05-24 00:07:52 +0200

    Migration to drop provider id from daily integration metric

---
+++ db/migrations/20250401130000_remove_provider_id_from_integration_daily_metrics.sql
@@ -0,0 +1,11 @@
+-- +goose Up
+-- +goose StatementBegin
+ALTER TABLE integration_daily_metrics
+DROP COLUMN provider_id;
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+ALTER TABLE integration_daily_metrics
+ADD COLUMN provider_id VARCHAR(50);
+-- +goose StatementEnd
